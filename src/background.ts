59aa00054711df834b7e7ed730daeb4d29b643aa
Marvin <petatemarvin26@gmail.com>
2025-04-24 10:08:42 +0800

    Feat: Add Some Content

--- src/background.ts
+++ src/background.ts
@@ -1,19 +1,34 @@
 // Invoke if the extension successfully installed or updated
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Successfully Installed!');
+chrome.runtime.onInstalled.addListener((tab) => {
+  console.log('Successfully', tab.reason);
 });
 
-// Intercept the onClick on ICON Extension
+// Intercept the onClick from ICON Extension
 chrome.action.onClicked.addListener((tab) => {
-  console.log(tab, '<---@');
-
   chrome.tabs.create({
-    url: chrome.runtime.getURL("web.html")
+    url: chrome.runtime.getURL('web.html')
   });
-  
   // This execute the scripts
   // chrome.scripting.executeScript({
   //   target: {tabId: tab.id!},
   //   files: ['content.js']
   // });
+});
+
+chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
+  // chrome.tabs.query({url: message.url}, (tabs) => {
+  //   sendResponse({tabs});
+  // });
+
+  if (message.type === 'CODE') {
+    chrome.tabs.query({url: message.url}, (tabs) => {
+      chrome.scripting.executeScript({
+        target: {tabId: tabs[0].id!},
+        func: (code) => new Function(code)(),
+        args: [message.payload]
+      });
+      sendResponse('Successful execute');
+    });
+    return true;
+  }
 });
--- src/content.ts
+++ src/content.ts
@@ -1,1 +1,11 @@
-document.body.style.display = 'none';
+async function extInit() {
+  const jsCode = `alert("Hello from Content.js!");`;
+
+  await chrome.runtime.sendMessage({
+    type: 'CODE',
+    url: window.location.href,
+    payload: jsCode
+  });
+}
+
+(() => extInit())();
